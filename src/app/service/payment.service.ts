b79a5dbaaf9291c172d3c9777a337d95fff774f0
Sumaila Bello <sumaila.bello@shootfish.xyz>
2021-02-16 12:17:07 +0100

    fixed rxjs observable

--- src/app/service/payment.service.ts
+++ src/app/service/payment.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Card } from '../models/card.model';
+import * as Rx from "rxjs/Rx";
+import { of, throwError } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,19 +11,39 @@ import { Card } from '../models/card.model';
 export class PaymentService {
 
 
-  baseUrl = "https://reqres.in/api";
+  baseUrl = "https://reqres.in/api/";
   constructor(public http: HttpClient) { }
 
+  loader: boolean = false;
   pay(body) {
-    console.log(body)
-    this.http.post<Card>(`${this.baseUrl}/users`, body)
-      .subscribe(data => {
-        console.log(data);
-        this.showToast('Operation successful', true);
-      }, error => {
-        console.log(error);
-        this.showToast("Operation failed", true);
-      })
+    this.toggleLoader(); //toggles loading animation
+    console.log(body);
+    const req = this.http.post<Card[]>(`${this.baseUrl}/users`, body);
+    req
+      .pipe(
+        catchError((err, src) => {
+            console.log('An error occured', err);
+            this.toggleLoader();
+            this.showToast("Operation failed", true);
+            console.log(src); //you can return this value to restat the observable again
+            return [];
+        }), 
+          finalize(() => console.log("Operation finalized to free resources."))
+        )
+          .subscribe(data => {
+              console.log(data);
+              this.toggleLoader();
+              this.showToast('Operation successful', true);
+            }, error => {
+              // this.toggleLoader();
+              console.log(error);
+              // this.showToast("Operation failed", true);
+            })
+  }
+
+  // toggles the display of laoding animation
+  toggleLoader() {
+    this.loader = !this.loader;
   }
 
   // toggles display of toast message
@@ -31,6 +54,6 @@ export class PaymentService {
     this.toast = status;
     setTimeout(()=> {
       this.showToast('', false); //turns off the toast
-    }, 5000)
+    }, 3000)
   }
 }
